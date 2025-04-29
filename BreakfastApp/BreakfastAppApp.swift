cecd3b25c91ea3fac1ba04829c2d8842ffc052a2
Baha Kardan <baha.mbk@gmail.com>
2025-04-29 14:32:01 +0300

    Initial Commit

---
+++ BreakfastApp/BreakfastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakfastAppApp.swift
+//  BreakfastApp
+//
+//  Created by Baha Kardan on 29.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct BreakfastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreakfastAppTests/BreakfastAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BreakfastAppTests.swift
+//  BreakfastAppTests
+//
+//  Created by Baha Kardan on 29.04.2025.
+//
+
+import Testing
+@testable import BreakfastApp
+
+struct BreakfastAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
